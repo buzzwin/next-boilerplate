e86af88ec7ab042c28b3441f54805ff51f5d6b40
Gunjan <gunjanv@yahoo.com>
2022-08-31 22:49:45 -0500

    Main Page

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,10 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className='items-center'>
-      <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </div>
+    <div className='flex flex-col items-center justify-center w-full h-screen'>
+      <h1 className='text-3xl'>The Boilerplate App</h1>
+      <h2 className='text-xl'>A minimalistic starter pack for Next JS development with Tailwind CSS</h2>
+  </div>
     
   )
 }
